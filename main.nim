c59d7e6926eb4e29de56ded566bd7532cd47147a
Franemar <franklin.e.marin@gmail.com>
2025-02-12 18:55:16 -0300

    Nim cmd test.

---
+++ main.nim
@@ -0,0 +1,9 @@
+import std/os
+
+# let cmd = 
+
+echo execShellCmd("pacman -Qqei > ~/.config/aconfmgr/pklist.txt")
+#               options: set[ProcessOption] = {poStdErrToStdOut, poUsePath};
+#               env: StringTableRef = nil; workingDir = ""; input = ""): tuple[
+#    output: string, exitCode: int] {.raises: [OSError, IOError], tags: [
+#    ExecIOEffect, ReadIOEffect, RootEffect], gcsafe, forbids: [].}
