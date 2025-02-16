e94825732b2c17399d6d06f8e989d94441fd6378
Franemar <franklin.e.marin@gmail.com>
2025-02-15 23:34:18 -0300

    main.nim and src/reason folder deleted.

--- main.nim
+++
@@ -1,9 +0,0 @@
-import std/os
-
-# let cmd = 
-
-assert execShellCmd("pacman -Qqei > ~/.config/aconfmgr/pklist.txt") == 0
-#               options: set[ProcessOption] = {poStdErrToStdOut, poUsePath};
-#               env: StringTableRef = nil; workingDir = ""; input = ""): tuple[
-#    output: string, exitCode: int] {.raises: [OSError, IOError], tags: [
-#    ExecIOEffect, ReadIOEffect, RootEffect], gcsafe, forbids: [].}
